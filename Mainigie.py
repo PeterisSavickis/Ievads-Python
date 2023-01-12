40c8a689357372bca663c96477a428656786c4c6
PeterisSavickis <30733475+PeterisSavickis@users.noreply.github.com>
2023-01-12 21:09:59 +0200

    Create Mainigie.py

---
+++ Mainigie.py
@@ -0,0 +1,19 @@
+x = "Hello World"	#Teksts (str)
+x = 20	            #skaitlis (int)
+x = 20.5	        #daļskaitlis (float)
+x = 1j	            #kombinētais datu tips (complex)
+x = True            #Boolean (True vai False)
+
+# Datu masīvi 
+x = ["apple", "banana", "cherry"]	list	
+x = ("apple", "banana", "cherry")	tuple	
+x = {"apple", "banana", "cherry"}	set	
+x = frozenset({"apple", "banana", "cherry"})	frozenset	
+
+
+vardnica = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "year": 2020
+}
